55346aaecf6672bd07aa16c8a5a1f569aacef608
JC Flores <tothenextcode@gmail.com>
2022-06-01 18:27:30 +0000

    Add count stats for all models and display on dashboard page in a table. Add button to redirect to Admin index page.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @products_count = Product.all.length
+    @categories_count = Category.all.length
+    @orders_count = Order.all.length
+    @line_items_count = LineItem.all.length
   end
 end
