e100e56487ee93a8653acf69ee63a85200390c0f
JC Flores <tothenextcode@gmail.com>
2022-06-02 20:33:20 +0000

    Fix basic authentication for dashboard where ENV variables use 'to_str' as suffixes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_str, password: ENV['ADMIN_PASSWORD'].to_str
 
   def show
+    @products_count = Product.all.length
+    @categories_count = Category.all.length
+    @orders_count = Order.all.length
+    @line_items_count = LineItem.all.length
   end
 end
