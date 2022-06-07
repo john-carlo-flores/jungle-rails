901339d80d5d61adb999148d3954c69574725463
JC Flores <tothenextcode@gmail.com>
2022-06-07 20:13:39 +0000

    Initialize new user using User.create! to test for existing user login. Also test registration and login sequence

--- config/initializers/cypress-rails.rb
+++ config/initializers/cypress-rails.rb
@@ -23,6 +23,14 @@ CypressRails.hooks.before_server_start do
     quantity: 18,
     price: 24.99
   })
+
+  user = User.create!({
+    first_name: 'Mowgli',
+    last_name: 'Man-cub',
+    email: 'test@test.com',
+    password: 'password',
+    password_confirmation: 'password'
+  })
 end
 
 CypressRails.hooks.after_transaction_start do
