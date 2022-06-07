6a4c71c9a9ce00bfa7e0f17d1c95a5a556e91aac
JC Flores <tothenextcode@gmail.com>
2022-06-07 01:43:55 +0000

    Call authenticate_with_credentials instead in session controller def create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.authenticate_with_credentials(params[:email], params[:password])
 
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user
+      session[:user_id] = @user.id
       redirect_to [:root]
     else
       render :new
