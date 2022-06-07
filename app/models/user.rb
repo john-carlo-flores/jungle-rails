65d9e6e03b0eb0cf135c5fb3a4b8990711ec4e6a
JC Flores <tothenextcode@gmail.com>
2022-06-07 01:12:04 +0000

    Add test for minimum password length requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :password, :password_confirmation
+  validates :password, :password_confirmation, length: { minimum: 6 }, presence: true
   validates_presence_of :email, :first_name, :last_name
   validates :email, :uniqueness => { case_sensitive: false }
 
