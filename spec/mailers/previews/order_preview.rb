11ed04e03d790fa599e2dc3d21bf7d72132d89a2
JC Flores <tothenextcode@gmail.com>
2022-06-07 22:22:13 +0000

    Implement preview for order to see email structure

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order
 class OrderPreview < ActionMailer::Preview
-
+  def receipt
+    OrderMailer.with(user: User.first, order: Order.first).receipt
+  end
 end
