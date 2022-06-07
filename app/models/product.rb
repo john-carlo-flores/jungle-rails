4ba9040209159b5f1ce6568024fcd754b2427438
JC Flores <tothenextcode@gmail.com>
2022-06-07 20:20:36 +0000

    Create helper sold_out to segregate business logic to model instead

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
+
+  def sold_out?
+    self.quantity < 1
+  end
 end
