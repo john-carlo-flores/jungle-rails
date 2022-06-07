9bf1bd17e8268d65499ca2c412a6bf06baa5a7d2
JC Flores <tothenextcode@gmail.com>
2022-06-07 04:59:54 +0000

    Add cypress spec tests for home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,13 @@
-describe('My First Cypress Test', function() {
-  it('Visits Home Page', function() {
-    //Visit the Demo QA Website
-    cy.visit("0.0.0.0:3000");
+describe("Testing Home Page", () => {
+  before(() => {
+    cy.visit("0.0.0.0:3000"); // Visit Home Page
   })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
