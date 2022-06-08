28c240f4ca89e1fe298c82aa7873fa05f0eff47e
JC Flores <tothenextcode@gmail.com>
2022-06-08 01:43:07 +0000

    List and style all reviews in products#show. Add star_ratings in Review model to display stars instead of integer

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,9 @@ class Review < ApplicationRecord
   belongs_to :user, validate: true
 
   validates_presence_of :description, :rating
+
+  def star_ratings
+    gold_stars = (1..self.rating).map {|rating| "⭐"}
+    gold_stars.join
+  end
 end
