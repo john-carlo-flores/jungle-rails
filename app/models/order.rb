54d8b268d07a519b36cbbca59eccbde5af75e0f1
JC Flores <tothenextcode@gmail.com>
2022-06-08 21:13:35 +0000

    Add after_create callback to adjust product quantity based in line_item quantity orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,14 @@ class Order < ApplicationRecord
 
   validates :stripe_charge_id, presence: true
 
+  after_create :adjust_product_quantities
+
+  private
+
+  def adjust_product_quantities
+    line_items.each do |line_item|
+      line_item.product.quantity = line_item.product.quantity - line_item.quantity
+      line_item.product.save
+    end
+  end
 end
