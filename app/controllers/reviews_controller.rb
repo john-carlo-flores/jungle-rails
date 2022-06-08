ee326ad1e7d4bbc531ee7e1bfad492f28f6e29a5
JC Flores <tothenextcode@gmail.com>
2022-06-08 19:12:44 +0000

    Implement reviews#create for new reviews. Add id to Reviews header to jump to when redirected back after create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,25 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.product = @product
+
+    @user = current_user
+    @review.user = @user
+    
+    if !@review.save
+      puts @review.errors.full_messages
+    end
+
+    redirect_to product_path(@product, anchor: 'reviews')
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
   end
 end
